3a1802a03759aec57297a15339a26738f3849465
Mücahit Bozbey <192163217+mucahitbozbey@users.noreply.github.com>
2025-03-18 02:02:02 +0300

    update

---
+++ script.js
@@ -0,0 +1,56 @@
+function handleCount() {
+    let count = document.getElementById('count');
+    let countNumber = Number(count.innerHTML);
+    countNumber++;
+    count.innerHTML = countNumber;
+    if(countNumber % 5 === 0) {
+        alert("Git Bi Dinlen");
+    }
+}
+
+const birdPlayAudio = document.getElementById('birdPlayAudio');
+const birdPlay = document.getElementById('birdPlay');
+
+birdPlay.addEventListener("click", function() {
+    if (birdPlayAudio.paused) {
+        birdPlayAudio.play();
+        birdPlay.className = "fa-solid fa-pause";
+    } else{
+        birdPlayAudio.pause();
+        birdPlay.className = "fa-solid fa-dove";
+    } 
+});
+
+const pianoPlayAudio = document.getElementById('pianoPlayAudio');
+const pianoPlay = document.getElementById('pianoPlay');
+
+pianoPlay.addEventListener("click", function() {
+    if (pianoPlayAudio.paused) {
+        pianoPlayAudio.play();
+        pianoPlay.className = "fa-solid fa-pause";
+    } else{
+        pianoPlayAudio.pause();
+        pianoPlay.className = "fa-solid fa-play";
+    } 
+});
+
+const rainPlayAudio = document.getElementById('rainPlayAudio');
+const rainPlay = document.getElementById('rainPlay');
+
+rainPlay.addEventListener("click", function() {
+    if (rainPlayAudio.paused) {
+        rainPlayAudio.play();
+        rainPlay.className = "fa-solid fa-pause";
+    } else{
+        rainPlayAudio.pause();
+        rainPlay.className = "fa-solid fa-cloud";
+    } 
+});
+
+const volumeInput = document.getElementById("volume-input");
+
+volumeInput.addEventListener("input", function () {
+    birdPlayAudio.volume = volumeInput.value / 100;
+    pianoPlayAudio.volume = volumeInput.value / 100;
+    rainPlayAudio.volume = volumeInput.value / 100;   
+});
