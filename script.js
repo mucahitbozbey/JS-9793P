a1aa21b39eb361773bed7a997226da51aa0d22dd
Mücahit Bozbey <192163217+mucahitbozbey@users.noreply.github.com>
2025-03-18 02:14:16 +0300

    update-2

--- script.js
+++ script.js
@@ -11,40 +11,58 @@ function handleCount() {
 const birdPlayAudio = document.getElementById('birdPlayAudio');
 const birdPlay = document.getElementById('birdPlay');
 
-birdPlay.addEventListener("click", function() {
+birdPlay.addEventListener("click", function () {
     if (birdPlayAudio.paused) {
         birdPlayAudio.play();
         birdPlay.className = "fa-solid fa-pause";
-    } else{
+
+        // Diğerlerini durdur
+        pianoPlayAudio.pause();
+        rainPlayAudio.pause();
+        pianoPlay.className = "fa-solid fa-play";
+        rainPlay.className = "fa-solid fa-cloud";
+    } else {
         birdPlayAudio.pause();
         birdPlay.className = "fa-solid fa-dove";
-    } 
+    }
 });
 
 const pianoPlayAudio = document.getElementById('pianoPlayAudio');
 const pianoPlay = document.getElementById('pianoPlay');
 
-pianoPlay.addEventListener("click", function() {
+pianoPlay.addEventListener("click", function () {
     if (pianoPlayAudio.paused) {
         pianoPlayAudio.play();
         pianoPlay.className = "fa-solid fa-pause";
-    } else{
+
+        // Diğerlerini durdur
+        birdPlayAudio.pause();
+        rainPlayAudio.pause();
+        birdPlay.className = "fa-solid fa-dove";
+        rainPlay.className = "fa-solid fa-cloud";
+    } else {
         pianoPlayAudio.pause();
         pianoPlay.className = "fa-solid fa-play";
-    } 
+    }
 });
 
 const rainPlayAudio = document.getElementById('rainPlayAudio');
 const rainPlay = document.getElementById('rainPlay');
 
-rainPlay.addEventListener("click", function() {
+rainPlay.addEventListener("click", function () {
     if (rainPlayAudio.paused) {
         rainPlayAudio.play();
         rainPlay.className = "fa-solid fa-pause";
-    } else{
+
+        // Diğerlerini durdur
+        birdPlayAudio.pause();
+        pianoPlayAudio.pause();
+        birdPlay.className = "fa-solid fa-dove";
+        pianoPlay.className = "fa-solid fa-play";
+    } else {
         rainPlayAudio.pause();
         rainPlay.className = "fa-solid fa-cloud";
-    } 
+    }
 });
 
 const volumeInput = document.getElementById("volume-input");
